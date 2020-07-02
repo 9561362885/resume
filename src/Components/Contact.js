ad688dbbe335cf1eb333bb3d17fcc7c407690e64
patilashishdx@gmail.com <patilashishdx@gmail.com>
2020-07-02 10:31:56 +0530

    Final

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,32 +1,6 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 
 class Contact extends Component {
-   constructor() {
-      super()
-
-      this.state = {
-         name: '',
-         email: '',
-         message: ''
-      }
-      this.handleChange = this.handleChange.bind(this)
-      this.handleSubmit = this.handleSubmit.bind(this)
-   }
-   handleChange = e => {
-      this.setState({ [e.target.name]: e.target.value })
-   }
-   async handleSubmit(e) {
-      e.preventDefault()
-
-      const {name, email, message} = this.state
-      // eslint-disable-next-line
-      const form = await axios.post('/api/form',{
-         name,
-         email,
-         message
-      })
-   }
 
   render() {
 
@@ -42,51 +16,70 @@ class Contact extends Component {
 
     return (
       <section id="contact">
+
          <div className="row section-head">
+
             <div className="two columns header-col">
+
                <h1><span>Get In Touch.</span></h1>
+
             </div>
+
             <div className="ten columns">
+
                   <p className="lead">{message}</p>
                   <br/>
                   <p className="lead warning">Warning: Contact form not yet configured, please contact me via normal email for now</p>
+
             </div>
+
          </div>
+
          <div className="row">
             <div className="eight columns">
-               <form action="" id="contactForm" name="contactForm" method="POST" onSubmit={this.handleSubmit}>
+
+               <form action="" method="post" id="contactForm" name="contactForm">
 					<fieldset>
+
                   <div>
 						   <label htmlFor="contactName">Name <span className="required">*</span></label>
-						   <input type="text" defaultValue="" size="35" id="contactName" name="contactName" value={this.state.name} onChange={this.handleChange}/>
+						   <input type="text" defaultValue="" size="35" id="contactName" name="contactName" onChange={this.handleChange}/>
                   </div>
+
                   <div>
 						   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-						   <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" value={this.state.email} onChange={this.handleChange}/>
+						   <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={this.handleChange}/>
                   </div>
+
                   <div>
 						   <label htmlFor="contactSubject">Subject</label>
 						   <input type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" onChange={this.handleChange}/>
                   </div>
+
                   <div>
                      <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                     <textarea cols="50" rows="15" id="contactMessage" name="contactMessage" value={this.state.message} onChange={this.handleChange}></textarea>
+                     <textarea cols="50" rows="15" id="contactMessage" name="contactMessage"></textarea>
                   </div>
+
                   <div>
-                     <button type="submit" className="submit">Submit</button>
+                     <button className="submit">Submit</button>
                      {/* <span id="image-loader">
                         <img alt="" src="images/loader.gif" />
                      </span> */}
                   </div>
 					</fieldset>
 				   </form>
+
            <div id="message-warning"> Error boy</div>
 				   <div id="message-success">
                   <i className="fa fa-check"></i>Your message was sent, thank you!<br />
 				   </div>
            </div>
+
+
             <aside className="four columns footer-widgets">
                <div className="widget widget_contact">
+
 					   <h4>Address and Phone</h4>
 					   <p className="address">
 						   {name}<br />
